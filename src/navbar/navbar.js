b888a7668991a50a4ad2827223a6e2bd165e5421
Radu <radu.p1995@yahoo.com>
2021-11-11 22:08:10 +0200

    master

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -6,16 +6,16 @@ function navbar() {
  return (
   <div>
    <Navbar className="myNav" fixed="top" expand="lg" >
-  <Container>
-    <Navbar.Brand className="titlu text-white"  href="#home">Hill-Valey</Navbar.Brand>
+  <Container >
+    <Navbar.Brand className="titlu text-black"  href="#home">Hill-Valey</Navbar.Brand>
     <Navbar.Toggle className="collaps" aria-controls="basic-navbar-nav" />
     <Navbar.Collapse className="collaps" id="basic-navbar-nav">
       <Nav className="mx-auto">
-        <Nav.Link className="text-white mx-5 linkuri" href="#home">Pensiune</Nav.Link>
-        <Nav.Link className="text-white mx-5 linkuri" href="#link">Camere</Nav.Link>
-        <Nav.Link className="text-white mx-5 linkuri" href="#home">Galerie</Nav.Link>
-        <Nav.Link className="text-white mx-5 linkuri" href="#link">Facilitati</Nav.Link>
-        <Nav.Link className="text-white mx-5 linkuri" href="#home">Contact</Nav.Link>
+        <Nav.Link className="text-black mx-5 linkuri" href="#home">Pensiune</Nav.Link>
+        <Nav.Link className="text-black mx-5 linkuri" href="#link">Camere</Nav.Link>
+        <Nav.Link className="text-black mx-5 linkuri" href="#home">Galerie</Nav.Link>
+        <Nav.Link className="text-black mx-5 linkuri" href="#link">Facilitati</Nav.Link>
+        <Nav.Link className="text-black mx-5 linkuri" href="#home">Contact</Nav.Link>
       
         {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
